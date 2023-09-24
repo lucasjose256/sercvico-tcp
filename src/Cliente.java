627030f86b056bc6b65cfae0e95de7a8f69c4135
lucasjose256 <79601444+lucasjose256@users.noreply.github.com>
2023-09-24 19:49:34 -0300

    first commit

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,6 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class Cliente {
@@ -8,17 +6,35 @@ public class Cliente {
         //Abre um conexão com o servidor
         Socket socket= new Socket("192.168.3.3",54321);
         //define a stream de saida de dados do cliente
-        DataOutputStream saida= new DataOutputStream(socket.getOutputStream());
+        // Fluxo de entrada para receber o arquivo
+        InputStream in = socket.getInputStream();
 
-        saida.writeUTF("lucas");
-        //define a stream de entrada de dados do cliente
+        // Caminho onde você deseja salvar o arquivo recebido
+        String filePath = "C:\\Users\\lucas\\IdeaProjects\\servico_tcp\\out/arquivo.txt";
+
+        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = in.read(buffer)) != -1) {
+            fileOutputStream.write(buffer, 0, bytesRead);
+        }
+        // Crie um objeto BufferedReader para ler a string do servidor
+        BufferedReader text = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        // Leia a string do servidor
+        String receivedMessage = text.readLine();
         DataInputStream entrada= new DataInputStream(socket.getInputStream());
         String mensagem= entrada.readUTF();
-        System.out.println(mensagem);
 
-        //fecha streams de entrada e saídad de dados
-        entrada.close();
-        saida.close();
+        System.out.println("Mensagem recebida do servidor: " + mensagem);
+
+        // Feche os recursos
+        in.close();
+        fileOutputStream.close();
+        socket.close();
+
+        System.out.println("Arquivo recebido com sucesso!");
 
         //fechar o socket
         socket.close();
--- src/ThreadSockets.java
+++ src/ThreadSockets.java
@@ -1,6 +1,7 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.util.Base64;
 
 public class ThreadSockets extends Thread {
     private Socket socket;
@@ -15,17 +16,46 @@ public class ThreadSockets extends Thread {
         System.out.println(Thread.currentThread().getName()); // Imprime o nome da thread atual.
 
         try {
-            DataInputStream entrada = new DataInputStream(socket.getInputStream()); // Cria um fluxo de entrada de dados a partir do socket.
-            String mensagem = entrada.readUTF(); // Lê uma mensagem do fluxo de entrada.
+            OutputStream out = socket.getOutputStream();
 
-            String novaMensagem = mensagem.toUpperCase(); // Converte a mensagem para maiúsculas.
+            // Caminho para o arquivo que você deseja enviar
+            String filePath = "C:\\Users\\lucas\\Downloads\\linux.txt";
 
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            // Obtém o hash como um array de bytes
+
+
+            File fileToSend = new File(filePath);
+
+            // Lê o arquivo e o envia para o cliente
+            FileInputStream fileInputStream = new FileInputStream(fileToSend);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+                md.update(buffer, 0, bytesRead);
+            }
+            byte[] hashBytes = md.digest();
+
+            // Converte o hash em uma representação hexadecimal
+            StringBuilder sb = new StringBuilder();
+            for (byte hashByte : hashBytes) {
+                sb.append(String.format("%02x", hashByte));
+            }
+
+            // Exibe o hash
+            String hashValue = sb.toString();
+            // Crie um objeto PrintWriter para enviar a string
             DataOutputStream saida = new DataOutputStream(socket.getOutputStream()); // Cria um fluxo de saída de dados para o socket.
-            saida.writeUTF(novaMensagem); // Escreve a mensagem em maiúsculas no fluxo de saída.
+            saida.writeUTF(hashValue);
+            System.out.println("Hash SHA-256 do arquivo: " + hashValue);
+            saida.close();
+            fileInputStream.close();
+            out.close();
+            socket.close();
 
-            entrada.close(); // Fecha o fluxo de entrada de dados.
-            saida.close(); // Fecha o fluxo de saída de dados.
-            socket.close(); // Fecha o socket, encerrando a conexão.
+            System.out.println("Arquivo enviado com sucesso!");
 
         } catch (Exception e) {
             e.printStackTrace(); // Trata qualquer exceção lançada durante o processo e imprime o rastreamento da pilha.
