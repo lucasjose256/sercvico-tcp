f3665543745ac83d5d8541448b94bee29b6cbfa7
thegok5 <112995416+thegok5@users.noreply.github.com>
2023-12-14 19:37:22 -0300

    .

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,12 +1,16 @@
+import javax.sound.midi.Sequence;
 import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Servidor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         try {
             DatagramSocket datagramSocket = new DatagramSocket(54321, InetAddress.getByName("192.168.18.6"));
             System.out.println("A porta 54321 foi aberta");
@@ -20,12 +24,68 @@ public class Servidor {
                 String nomeArquivo = new String(receivePacket.getData(), 0, receivePacket.getLength());
                 System.out.println("Received request for file: " + nomeArquivo);
 
+                if(nomeArquivo.equals("-1"))
+                    RepairFile(datagramSocket, receivePacket.getAddress(), receivePacket.getPort(), nomeArquivo);
+                else
                 sendFileToClient(datagramSocket, receivePacket.getAddress(), receivePacket.getPort(), nomeArquivo);
             }
         } catch (IOException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
     }
+    private static void RepairFile(DatagramSocket socket, InetAddress clientAddress, int clientPort, String fileName)
+            throws IOException, NoSuchAlgorithmException, InterruptedException {
+
+        byte[] receiveBuffer = new byte[4096];
+        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+        socket.receive(receivePacket);
+        String nomeArquivo = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        File arquivo = new File(nomeArquivo);
+        byte[] fileData = readFileToByteArray(arquivo);
+        String sequence = "";
+        while(true) {
+            socket.receive(receivePacket);
+            String t = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            if (t.equals("-1"))
+                break;
+            sequence += t;
+        }
+        System.out.println(sequence);
+        String[] numbersArray = sequence.split(",");
+
+        int[] intArray = new int[numbersArray.length];
+        for (int i = 0; i < numbersArray.length; i++) {
+            intArray[i] = Integer.parseInt(numbersArray[i]);
+        }
+        try {
+            for (int i = 0; i < intArray.length; i++) {
+                int aux = 0;
+                aux = intArray[i];
+                String a = "" + aux;
+                System.out.println("A: " + a);
+                byte[] byt = a.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(byt, byt.length, clientAddress, clientPort);
+                socket.send(sendPacket);
+                int size = 0;
+                if (fileData.length < (aux * 1024 + 1024) && fileData.length % 1024 != 0 && fileData.length - (aux * 1024) > 0) {
+                    size = fileData.length - (aux * 1024);
+                    System.out.println("Size A: " + size);
+                } else {
+                    size = 1024;
+                    System.out.println("Size B: " + size);
+                }
+                byte[] send = new byte[size];
+                System.arraycopy(fileData, aux * 1024, send, 0, size);
+                sendPacket = new DatagramPacket(send, send.length, clientAddress, clientPort);
+                socket.send(sendPacket);
+                Thread.sleep(10);
+            }
+            Path path = Path.of("teste.png");
+            Files.write(path, fileData, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        }
+        catch (Exception e){return;}
+    }
+
 
     private static void sendFileToClient(DatagramSocket socket, InetAddress clientAddress, int clientPort, String fileName)
             throws IOException, NoSuchAlgorithmException {
@@ -40,57 +100,59 @@ public class Servidor {
             long tamanhoDoArquivo = arquivo.length();
             String hashDoArquivo = calcularHash(arquivo);
 
-            // Read the file content into a byte array
-
-            // Calculate checksum
             byte[] fileData = readFileToByteArray(arquivo);
             byte checksum = calculateChecksum(fileData);
 
-            // Prepare the response message
             String response = "Nome do arquivo: " + nomeDoArquivo +
                     "\n" + "Tamanho: " + tamanhoDoArquivo + " bytes" +
                     "\n" + "Hash: " + hashDoArquivo + "\n" + "Checksum: " + ((int) checksum & 0xFF) + "\n" + "Status: ok" + "\n";
 
-            // Convert the response message to a byte array
             byte[] responseData = response.getBytes();
 
-            // Create a DatagramPacket to send the response to the client
             DatagramPacket sendPacket = new DatagramPacket(responseData, responseData.length, clientAddress, clientPort);
+            socket.send(sendPacket);
+            response = "" + ((int) checksum & 0xFF);
+            responseData = response.getBytes();
+            sendPacket = new DatagramPacket(responseData, responseData.length, clientAddress, clientPort);
             socket.send(sendPacket);
 
             // Create a DatagramPacket to send the file data to the client
-
-
+            String aux = "" + ((int)Math.ceil(fileData.length / 1024) + 1);
+            DatagramPacket indexT = new DatagramPacket(aux.getBytes(), aux.getBytes().length, clientAddress, clientPort);
+            socket.send(indexT);
             try (FileInputStream fis = new FileInputStream(arquivo);
                  ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-                byte[] buffer = new byte[1024];
+                byte[] buffer;
                 int bytesRead = 0;
-                while ((bytesRead = fis.read(buffer)) != -1) {
-                    bos.write(buffer, 0, bytesRead);
+                int i = 0;
+                while (i < Math.floor(fileData.length/1024)) {
+                    buffer = new byte[1024];
+                    String ind = "" + i;
+                    DatagramPacket index = new DatagramPacket(ind.getBytes(), ind.getBytes().length, clientAddress, clientPort);
+                    socket.send(index);
+                    System.arraycopy(fileData, i*1024, buffer, 0, buffer.length);
                     DatagramPacket filePacket = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
-                    System.out.println("Bytes: " + bytesRead);
                     socket.send(filePacket);
                     try {
-                        Thread.sleep(500);
+                        Thread.sleep(10);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
-                    buffer = new byte[1024];
+                    i++;
+               }
+                if(fileData.length % 1024 != 0)
+                {
+                    String ind = "" + i;
+                    DatagramPacket index = new DatagramPacket(ind.getBytes(), ind.getBytes().length, clientAddress, clientPort);
+                    socket.send(index);
+                    buffer = new byte[fileData.length - (int)Math.floor(fileData.length/1024)*1024];
+                    System.arraycopy(fileData, i*1024, buffer, 0, buffer.length);
+                    DatagramPacket filePacket = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
+                    socket.send(filePacket);
                 }
             }
-
-
-
-
-
-
-
-            /*DatagramPacket filePacket = new DatagramPacket(fileData, fileData.length, clientAddress, clientPort);
-            socket.send(filePacket);*/
-
             System.out.println("File sent to client");
         } else {
-            // If the file does not exist, send an error response to the client
             String response = "Status: arquivo inexistente";
             byte[] responseData = response.getBytes();
             DatagramPacket errorPacket = new DatagramPacket(responseData, responseData.length, clientAddress, clientPort);
--- src/janela.java
+++ src/janela.java
@@ -3,10 +3,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.net.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
 
 class Janela extends JFrame {
 
@@ -18,6 +19,8 @@ class Janela extends JFrame {
     private JTextArea infoArquivoArea;
     private boolean conexao;
     private boolean arquivoRecebido;
+
+    private boolean breakConnection;
     /*private DataOutputStream saida;
 
     private DataInputStream entrada;
@@ -30,6 +33,7 @@ class Janela extends JFrame {
 
         conexao = true;
         arquivoRecebido = false;
+        breakConnection = false;
         Container janela = getContentPane();
         setLayout(null);
 
@@ -57,7 +61,9 @@ class Janela extends JFrame {
         scrollPane.setBounds(50, 120, 300, 100);
 
         JButton buttonSair = new JButton("Sair");
-        buttonSair.setBounds(50, 240, 100, 30);
+        buttonSair.setBounds(250, 240, 100, 30);
+        JButton breakCon = new JButton("Encerrar Conexao");
+        breakCon.setBounds(50, 240, 150, 30);
 
         janela.add(labelNomeArquivo);
         janela.add(nomeArquivoField);
@@ -66,9 +72,14 @@ class Janela extends JFrame {
         janela.add(buttonSolicitar);
         janela.add(scrollPane);
         janela.add(buttonSair);
+        janela.add(breakCon);
         janela.add(labelIP);
         janela.add(labelPorta);
-
+        breakCon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                breakConnection = true;
+            }
+        });
         buttonSolicitar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String nomeArquivo = nomeArquivoField.getText();
@@ -77,7 +88,7 @@ class Janela extends JFrame {
                 System.out.println("solicitado nome " + nomeArquivoField.getText());
                 try {
                     solicitarArquivo(nomeArquivo, IP, Porta);
-                } catch (IOException ex) {
+                } catch (IOException | InterruptedException ex) {
                     throw new RuntimeException(ex);
                 }
             }
@@ -101,7 +112,7 @@ class Janela extends JFrame {
         infoArquivoArea.setText(info);
     }
 
-    public void solicitarArquivo(String nomeArquivo, String IP, String Porta) throws IOException {
+    public void solicitarArquivo(String nomeArquivo, String IP, String Porta) throws IOException, InterruptedException {
         System.out.println("Arquivo: " + nomeArquivo + " Solicitado em: " + IP + ":" + Porta);
         DatagramSocket socket = new DatagramSocket();
         InetAddress serverAddress = InetAddress.getByName(IP);
@@ -121,40 +132,130 @@ class Janela extends JFrame {
         // Convert the received data to a string and print it
         String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
         System.out.println("Response from server: " + response);
-        /*try (FileOutputStream fos = new FileOutputStream("arquivoSalvo.txt")) {
-            fos.write(receiveBuffer);
-        }*/
-
-        /*socket.receive(receivePacket);
-        String r = new String(receivePacket.getData(), 0, receivePacket.getLength());
-        System.out.println(r);
-
-        // Close the socket*/
-        FileOutputStream fos = new FileOutputStream("arquivoSalvo.txt");
-        int i = 0;
+        if(response.contains("arquivo inexistente")) {
+            socket.close();
+            return;
+        }
+        int size = Integer.parseInt((response.replaceAll("(?s).*Tamanho: ", "")).replaceAll(" bytes(?s).*", ""));
+        socket.receive(receivePacket);
+        String checkSumserver = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        socket.receive(receivePacket);
+        String tamanho = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println("Teste: " + tamanho);
+        int siz = Integer.parseInt(tamanho);
+        System.out.println("Numeros de buffers: " + siz);
+        int buffercheck[] = new int[siz];
+        byte[] file = new byte[size];
+        String save = "image.png";
+        FileOutputStream fos = new FileOutputStream(save);
         socket.setSoTimeout(2000);
-        while (true) {
-            try {socket.receive(receivePacket);
-                //String r = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                System.out.println(i);
-                i++;
+        int z = 0;
+        while (!breakConnection) {
+            Random random = new Random();
+            Thread.sleep(random.nextInt(150));
+            try {
+                socket.receive(receivePacket);
+                int d = Integer.parseInt(new String(receivePacket.getData(), 0, receivePacket.getLength()));
+                System.out.println("Recebi: " + d);
+                System.out.println(d);
+                buffercheck[d] = 1;
+                socket.receive(receivePacket);
                 String r = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                System.out.println(r.length());
-                fos.write(receivePacket.getData(), 0, receivePacket.getLength());
+                //fos.write(receivePacket.getData(), 0, receivePacket.getLength());
+
+                System.arraycopy(receivePacket.getData(), 0, file, d*1024, receivePacket.getLength());
+                //if(z > 15)
+                //    break;
                 if (receivePacket.getLength() == 0 || receivePacket.getData().length == 0) {
                     break;
                 }
-                System.out.println("Loop");
             }
             catch (Exception e)
             {break;}
         }
         System.out.println("Saiu");
+        fos.write(file);
         fos.close();
-        byte[] checkFile = readFileToByteArray(new File("arquivoSalvo.txt"));
+        byte[] checkFile = readFileToByteArray(new File(save));
         byte check = calculateChecksum(checkFile);
+        String checksumClient = "" + ((int) check & 0xFF);
         System.out.println(((int) check & 0xFF));
+        breakConnection = false;
         socket.close();
+        String sequence = "";
+        while(!checkSumserver.equals(checksumClient)) {
+            for (int j = 0; j < siz; j++) {
+                if (buffercheck[j] == 0)
+                    sequence += j + ",";
+            }
+            sequence = sequence.substring(0, sequence.length() - 1);
+            String sinal = "-1";
+            socket = new DatagramSocket();
+            socket.setSoTimeout(10000);
+            byte[] byt = sinal.getBytes();
+            sendPacket = new DatagramPacket(byt, byt.length, serverAddress, serverPort);
+            socket.send(sendPacket);
+
+            byt = nomeArquivo.getBytes();
+            sendPacket = new DatagramPacket(byt, byt.length, serverAddress, serverPort);
+            socket.send(sendPacket);
+
+
+            byte[] data = sequence.getBytes();
+
+            int chunkSize = 1024;
+            int totalChunks = (data.length + chunkSize - 1) / chunkSize;
+
+            for (int k = 0; k < totalChunks; k++) {
+                int start = k * chunkSize;
+                int end = Math.min((k + 1) * chunkSize, data.length);
+
+                byte[] chunk = new byte[end - start];
+                System.arraycopy(data, start, chunk, 0, chunk.length);
+
+                sendPacket = new DatagramPacket(chunk, chunk.length, serverAddress, serverPort);
+                socket.send(sendPacket);
+            }
+            data = sinal.getBytes();
+            sendPacket = new DatagramPacket(data, data.length, serverAddress, serverPort);
+            socket.send(sendPacket);
+            while(true) {
+                try {
+                    socket.receive(receivePacket);
+
+                    String a = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    System.out.println("Pos: " + a);
+                    if(a.length() > 30)
+                        socket.receive(receivePacket);
+                        a = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    //System.out.println(a);
+                    int pos = Integer.parseInt(a);
+                    if(buffercheck[pos] != 1) {
+                        socket.receive(receivePacket);
+                        int length = receivePacket.getLength();
+                        System.arraycopy(receivePacket.getData(), 0, file, pos * 1024, length);
+                        System.out.println("Escreveu tamanho:" + length);
+                        buffercheck[pos] = 1;
+                    }
+                } catch (Exception e) {
+                    break;
+                }
+
+            }
+            System.out.println("Fim");
+            Path path = FileSystems.getDefault().getPath(save);
+            Files.delete(path);
+            fos = new FileOutputStream(save);
+            fos.write(file);
+            fos.close();
+            checkFile = readFileToByteArray(new File(save));
+            check = calculateChecksum(checkFile);
+            checksumClient = "" + ((int) check & 0xFF);
+            System.out.println("checksumClient: " + checksumClient + " checksumServer: " + checkSumserver);
+            socket.close();
+        }
+        //fos.write(file);
+        //fos.close();
     }
     private static byte[] readFileToByteArray(File file) throws IOException {
         try (FileInputStream fis = new FileInputStream(file);
